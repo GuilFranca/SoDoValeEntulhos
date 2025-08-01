e3a43012c6f9e17c6ebc8c7bc105cef216e4549a
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-08-01 15:30:59 -0300

    animações de cards inseridas

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,3 +1,15 @@
 const cards = document.querySelectorAll('.card__nosssos__servicos');
+let animacao = false;
 
-// const observer = document.documentElement.scroll
+
+window.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const telaLarga = window.innerWidth > 768;
+
+    if(scrollTop > 90 && telaLarga && !animacao) {
+        cards.forEach(card => {
+            card.classList.add('animate__animated', 'animate__backInLeft');
+        });
+        animacao = true;
+    }
+});
